cddaef91465d7dccdc4ecff834c5773d295a8b0a
MichelleAguirreP <168683240+MichelleAguirreP@users.noreply.github.com>
2024-05-02 13:40:24 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,13 @@ module.exports = defineConfig({
     },
   },
   // Configuración del navegador
-  browser: "edge"
+  browser: "edge",
+
+  // Configuración de permisos y seguridad
+  chromeWebSecurity: false, // Desactiva la seguridad web de Chrome (puede ser necesario para ciertos escenarios de prueba)
+  env: {
+    // Configura variables de entorno para ajustar permisos
+    CYPRESS_ALLOW_INSECURE_CONNECTIONS: true, // Permite conexiones inseguras
+    CYPRESS_INTERNAL_FORCE_PORT: "63986", // Forzar el puerto interno para evitar problemas de conexión
+  },
 });
